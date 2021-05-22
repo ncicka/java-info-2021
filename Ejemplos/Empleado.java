c51ba1833243058dd0dca3fdc387679949578816
Noemi Marina Cicka <ncicka@gmail.com>
2021-05-22 14:51:38 -0300

    Primeros Ejemplos Informa 2021

---
+++ Ejemplos/Empleado.java
@@ -0,0 +1,24 @@
+public abstract class Empleado {
+
+    private int dni;
+    private String nombre;
+    private String apellido;
+
+    public Empleado(int dni, String nombre, String apellido){
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public abstract int calcularSueldo();
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        //return super.toString();
+        return String.valueOf(this.dni)+
+        " - "+this.nombre +
+        " - "+this.apellido ;
+    }
+    
+}
