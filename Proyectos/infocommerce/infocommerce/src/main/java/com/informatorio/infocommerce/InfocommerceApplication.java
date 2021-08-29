d34d9b642c9942afdba6cfc5b6fbdc7e30b9bd00
ncicka <ncicka@gmail.com>
2021-08-29 11:27:37 -0300

    INICIAL - Commit inicial Proyecto Final

---
+++ Proyectos/infocommerce/infocommerce/src/main/java/com/informatorio/infocommerce/InfocommerceApplication.java
@@ -0,0 +1,13 @@
+package com.informatorio.infocommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfocommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfocommerceApplication.class, args);
+	}
+
+}
